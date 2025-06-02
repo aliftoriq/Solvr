29a0035b986b06434b4d1c5584a5458be2035ffb
aliftoriq <aliftoriq52@gmail.com>
2025-06-03 00:45:49 +0700

    Update cors

--- src/main/java/id/co/bcaf/solvr/controller/AuthController.java
+++ src/main/java/id/co/bcaf/solvr/controller/AuthController.java
@@ -132,8 +132,8 @@ public class AuthController {
         return ResponseEntity.ok(new ResponseTemplate(200, "Verifikasi Email Terkirim", httpResponse));
     }
 
-    @PostMapping("/verify")
-    public ResponseEntity<?> verifyUser(@RequestBody String token) {
+    @GetMapping("/verify")
+    public ResponseEntity<?> verifyUser(@RequestParam("token") String token) {
         authService.verifyEmail(token);
         return ResponseEntity.ok(new ResponseTemplate(200, "Email verified successfully.", null));
     }
