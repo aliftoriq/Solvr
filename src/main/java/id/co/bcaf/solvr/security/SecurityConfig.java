3f6a9a84a8a99cb5823e91b2829bb01337db7363
aliftoriq <aliftoriq52@gmail.com>
2025-06-02 17:25:22 +0700

    update permit

--- src/main/java/id/co/bcaf/solvr/security/SecurityConfig.java
+++ src/main/java/id/co/bcaf/solvr/security/SecurityConfig.java
@@ -32,7 +32,27 @@ public class SecurityConfig {
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/", "/home", "/css/**", "/js/**").permitAll()
-                        .requestMatchers("/api/v1/auth/register", "/api/v1/auth/login", "api/v1/auth/verify", "api/v1/auth/reset-password", "/api/v1/auth/forget-password", "/api/v1/auth/change-password", "/api/v1/auth/firebase-login","/api/v1/plafon/all", "/api/v1/notification").permitAll()
+                        .requestMatchers(
+                                "/api/v1/auth/register",
+                                "/api/v1/auth/login",
+                                "/api/v1/auth/verify",
+                                "/api/v1/auth/reset-password",
+                                "/api/v1/auth/forget-password",
+                                "/api/v1/auth/change-password",
+                                "/api/v1/auth/firebase-login",
+                                "/api/v1/plafon/all",
+                                "/api/v1/notification",
+
+                                "/be/api/v1/auth/register",
+                                "/be/api/v1/auth/login",
+                                "/be/api/v1/auth/verify",
+                                "/be/api/v1/auth/reset-password",
+                                "/be/api/v1/auth/forget-password",
+                                "/be/api/v1/auth/change-password",
+                                "/be/api/v1/auth/firebase-login",
+                                "/be/api/v1/plafon/all",
+                                "/be/api/v1/notification"
+                        ).permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
